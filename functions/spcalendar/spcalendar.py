40feb2c493e57289af82b664547a1a8c87459809
Yash0510 <yash.mishra@sparkeighteen.com>
2022-04-14 10:45:32 +0530

    Updated test.py and spcalendar.py

--- functions/spcalendar/spcalendar.py
+++ functions/spcalendar/spcalendar.py
@@ -1,8 +1,49 @@
+from calendar import monthrange
+
+
 def is_leap(year):
     """
     Description: Given an integer year in the input, it should return a boolean
-    signifying whether the given year is a year or not.
+    signifying whether the given year is a leap year or not.
     Input: year (integer)
     Ouput: boolean (leap or not)
     """
     return ((year % 4 == 0) and (year % 100 != 0)) or (year % 400 == 0)
+
+
+def number_of_days_in_month(year=0, month=0):
+    return monthrange(year, month)[1]
+
+
+def odd_days_in_month(month=0):
+    """
+    Description: Given an integer month in input,it should return an integer 
+    signifying the total no. of odd days in the month.
+    Input: month(integer)
+    Output: integer(total no of odd days)
+    """
+    count_odd = 0
+    for day in month:
+        if day % 2 != 0:
+            count_odd += 1
+    return count_odd
+
+
+def odd_days_in_century(century=0):
+    """
+    Description: Given an integer century in input,it should return an integer
+    signifying the total no. of odd days in a century.
+    Input: century(ineger)
+    Output: integer(total no of odd days in a century)
+    """
+    count_odd = 0
+    count_leap = 0
+    for year in century:
+        if is_leap(year) == True:
+            count_leap += 1
+    total_days = 36500 + count_leap
+    for day in total_days:
+        if day % 2 != 0:
+            count_odd += 1
+
+    return count_odd
--- functions/test.py
+++ functions/test.py
@@ -1,5 +1,10 @@
 from faker import Faker
 from spcalendar.spcalendar import is_leap
+from spcalendar.spcalendar import number_of_days_in_month
+from spcalendar.spcalendar import odd_days_in_month
+from spcalendar.spcalendar import odd_days_in_century
+
+import random
 
 fake = Faker()
 
@@ -12,3 +17,8 @@ dt = generate_random_date()
 print(f"Date: {dt}")
 
 print(f"Is leap year? - {is_leap(dt.year)}")
+print(
+    f'The number of days in the month is- {number_of_days_in_month(dt.year,dt.month)}')
+print(f'Total odd days in month {dt.month} is {odd_days_in_month(dt.month)}')
+century = random.randrange(1, 22)*100
+print(f'The no. of odd days in {century} is {odd_days_in_century(century)}')

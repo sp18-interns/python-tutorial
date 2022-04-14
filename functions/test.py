e4c12fba97b3b1a8ba864ad46cfc836d9b26ea74
parag-intern <parag.gunjal@sparkeighteen.com>
2022-04-14 11:24:08 +0530

    New commit

--- functions/test.py
+++ functions/test.py
@@ -1,7 +1,8 @@
 from faker import Faker
 from spcalendar.spcalendar import is_leap
 from spcalendar.spcalendar import decode_month
-
+from spcalendar.spcalendar import decode_day
+import datetime
 fake = Faker()
 
 def generate_random_date():
@@ -11,5 +12,7 @@ dt = generate_random_date()
 print(f"Date: {dt}")
 
 print(f"Is leap year? - {is_leap(dt.year)}")
-print(f"Decode Month to string :  - {decode_month(10)}")
 
+print(f"Decode Month to string :  - {decode_month(dt.month)}")
+
+#print(f"the weekday is : - {dt.dayofweek}")
